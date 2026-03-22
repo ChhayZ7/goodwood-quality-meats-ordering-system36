780e871f06a2dd7d6d30df25e982d4cbd3ab7c9b
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2026-03-22 12:10:13 +1030

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// Root Layout Page - navbar, footer, font, cart provider
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
