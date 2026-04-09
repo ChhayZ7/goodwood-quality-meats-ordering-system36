4fea1ebc515dd47349d5be9ce9feeb07adb5108b
patvn002 <vraj.n.patel@student.adelaide.edu.au>
2026-04-09 12:00:34 +0930

    add basic footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import GoldDivider from './GoldDivider'
+
+
+export default function Footer() {
+  return (
+    <footer>
+      <GoldDivider />
+
+      <section>
+        <h2>Opening Hours</h2>
+        <p>Monday to Friday: 7am - 5:30pm</p>
+        <p>Saturday: 7am - 12:00pm</p>
+        <p>Sunday: Closed</p>
+      </section>
+
+      <section>
+        <h2>Visit Our Store</h2>
+        <p>121 Goodwood Road, Goodwood</p>
+        <p>Phone: 08 8271 4183</p>
+      </section>
+
+      <section>
+        <p>Goodwood Quality Meats</p>
+      </section>
+    </footer>
+  )
+}
