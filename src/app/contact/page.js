b72244a98737ad884cbf88d2348e5b9053f8cabc
Rohini Goyal <goyry006@mymail.unisa.edu.au>
2026-04-09 12:59:46 +0930

    Add the location for the contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaClock } from "react-icons/fa";
+import { FiMapPin, FiPhone, FiMail, FiClock } from "react-icons/fi";
 
 export default function ContactPage() {
   return (
@@ -17,69 +17,69 @@ export default function ContactPage() {
       {/* CENTER CONTAINER */}
       <div className="max-w-7xl mx-auto px-10">
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-20 items-start">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-start">
 
           {/* LEFT SIDE */}
           <div className="pl-6">
 
-            <h2 className="text-3xl font-semibold mb-10" style={{ color: "#222" }}>
+            <h2 className="text-3xl font-bold mb-10" style={{ color: "#222" }}>
               Get in Touch
             </h2>
 
             {/* ADDRESS */}
             <div className="flex items-start gap-4 mb-8">
-              <FaMapMarkerAlt style={{ color: "#7b1e1e" }} size={20} />
+              <FiMapPin className="text-[#7b1e1e] mt-1" size={22} />
               <div>
-                <h3 style={{ color: "#000" }}>Visit Our Store</h3>
-                <p style={{ color: "#555" }}>121 Goodwood Road</p>
-                <p style={{ color: "#555" }}>Goodwood, SA 5034</p>
-                <p style={{ color: "#555" }}>Australia</p>
+                <h3 style={{ color: "#000" }} className="text-2xl mb-2"><b>Visit Our Store</b></h3>
+                <p style={{ color: "#555" }} className="text-2xl">121 Goodwood Road</p>
+                <p style={{ color: "#555" }}className="text-2xl">Goodwood, SA 5034</p>
+                <p style={{ color: "#555" }}className="text-2xl">Australia</p>
               </div>
             </div>
 
             {/* PHONE */}
             <div className="flex items-start gap-4 mb-8">
-              <FaPhoneAlt style={{ color: "#7b1e1e" }} size={20} />
+              <FiPhone className="text-[#7b1e1e] mt-1" size={22} />
               <div>
-                <h3 style={{ color: "#000" }}>Call Us</h3>
-                <p style={{ color: "#555" }}>08 8271 4183</p>
+                <h3 style={{ color: "#000" }} className="text-2xl mb-2"><b>Call Us</b></h3>
+                <p style={{ color: "#555" }}className="text-2xl">08 8271 4183</p>
               </div>
             </div>
 
             {/* EMAIL */}
             <div className="flex items-start gap-4 mb-8">
-              <FaEnvelope style={{ color: "#7b1e1e" }} size={20} />
+              <FiMail className="text-[#7b1e1e] mt-1" size={22} />
               <div>
-                <h3 style={{ color: "#000" }}>Email</h3>
-                <p style={{ color: "#555" }}>info@goodwoodmeats.com.au</p>
+                <h3 style={{ color: "#000" }}className="text-2xl mb-2" ><b>Email</b></h3>
+                <p style={{ color: "#555" }} className="text-2xl" >info@goodwoodmeats.com.au</p>
               </div>
             </div>
 
             {/* HOURS */}
             <div className="flex items-start gap-4">
-              <FaClock style={{ color: "#7b1e1e" }} size={20} />
+              <FiClock className="text-[#7b1e1e] mt-1" size={22} />
               <div>
-                <h3 style={{ color: "#000" }}>Opening Hours</h3>
-                <p style={{ color: "#555" }}>Monday – Friday: 7am – 5:30pm</p>
-                <p style={{ color: "#555" }}>Saturday: 7am – 12pm</p>
-                <p style={{ color: "#555" }}>Sunday: Closed</p>
+                <h3 style={{ color: "#000" }} className="text-2xl mb-2"> <b>Opening Hours</b></h3>
+                <p style={{ color: "#555" }} className="text-2xl">Monday – Friday: 7am – 5:30pm</p>
+                <p style={{ color: "#555" }} className="text-2xl">Saturday: 7am – 12pm</p>
+                <p style={{ color: "#555" }} className="text-2xl">Sunday: Closed</p>
               </div>
             </div>
 
           </div>
 
-         <div className="self-start">
-            <a
-              href="https://www.google.com/maps?q=121+Goodwood+Road+Goodwood+SA"
-              target="_blank"
-            >
-              <img
-                src="/map.png"
-                alt="Map"
-                className="w-[350px] h-[220px] object-cover rounded-2xl shadow-sm ml-auto"
-              />
-            </a>
-          </div>
+         <div className="flex justify-center md:justify-end">
+          <a
+            href="https://www.google.com/maps?q=121+Goodwood+Road+Goodwood+SA"
+            target="_blank"
+          >
+            <img
+              src="/map.png"
+              alt="Map"
+              className="w-[980px] h-[600px] object-cover rounded-2xl shadow-sm"
+            />
+          </a>
+        </div>
 
         </div>
       </div>
