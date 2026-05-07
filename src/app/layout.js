0a4f791b310263d3389a07209ed2518fd1cc3b0f
evelyn <133929343+evelyn-giahan@users.noreply.github.com>
2026-05-07 10:10:47 +0930

    changed the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,8 +24,8 @@ export default function RootLayout({ children }) {
     >
       
       <body className="min-h-full flex flex-col">
+        <CartProvider>
           <Navbar />
-          <CartProvider>
           <main className="flex-1 ">
             {children}
           </main>
